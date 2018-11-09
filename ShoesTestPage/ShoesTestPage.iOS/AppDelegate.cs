82177b5af7114e8d65901c2bf954a808b74ffa63
Daniil Mikhin <damihin@gmail.com>
2018-11-10 00:45:05 +0700

    iOS polishes

--- ShoesTestPage/ShoesTestPage.iOS/AppDelegate.cs
+++ ShoesTestPage/ShoesTestPage.iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using KeyboardOverlap.Forms.Plugin.iOSUnified;
 using UIKit;
 
 namespace ShoesTestPage.iOS
@@ -23,6 +24,7 @@ namespace ShoesTestPage.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            KeyboardOverlapRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
---
+++ ShoesTestPage/ShoesTestPage.iOS/Renderers/LightEntryRenderer.cs
@@ -0,0 +1,19 @@
+﻿using ShoesTestPage.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(LightEntryRenderer))]
+
+namespace ShoesTestPage.iOS.Renderers
+{
+    public class LightEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (this.Control == null) return;
+            this.Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
