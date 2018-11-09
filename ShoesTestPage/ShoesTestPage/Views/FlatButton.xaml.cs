904e970b9ef0fa1125bff9bc6508b24ee3a1a721
Daniil.Mikhin <damihin@gmail.com>
2018-11-10 00:13:18 +0700

    Initial

---
+++ ShoesTestPage/ShoesTestPage/Views/FlatButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShoesTestPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FlatButton : ContentView
+	{
+        public string Text
+        {
+            get => textLabel.Text;
+            set => textLabel.Text = value;
+        }
+
+        public Color Color
+        {
+            get => backgroundFrame.BackgroundColor;
+            set => backgroundFrame.BackgroundColor = value;
+        }
+
+        public float CornerRadius
+        {
+            get => backgroundFrame.CornerRadius;
+            set => backgroundFrame.CornerRadius = value;
+        }
+
+        public FlatButton ()
+		{
+			InitializeComponent ();
+		}
+    }
+}
---
+++ ShoesTestPage/ShoesTestPage/Views/MessageView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShoesTestPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MessageView : ContentView
+	{
+        public string Text
+        {
+            get => textLabel.Text;
+            set => textLabel.Text = value;
+        }
+
+        public Color Color
+        {
+            get => backgroundFrame.BackgroundColor;
+            set
+            {
+                backgroundFrame.BackgroundColor = value;
+                cornerView.BackgroundColor = value;
+            }
+        }
+
+        public Color TextColor
+        {
+            get => textLabel.TextColor;
+            set => textLabel.TextColor = value;
+        }
+
+        public MessageView()
+		{
+			InitializeComponent ();
+		}
+    }
+}
